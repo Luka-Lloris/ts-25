c7e625a09e62a8d37fadacf9ce070cd708e82ecf
Luka <luka.blacklime@gmail.com>
2026-05-18 16:23:20 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ts=25">
+    <BrowserRouter basename="/ts-25">
       <App />
     </BrowserRouter>
   </React.StrictMode>
